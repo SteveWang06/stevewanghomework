e5fb1eb734c9292374d5d085d340b28d3bf4183e
SteveWang <thanhphuoc1306vn@gmail.com>
2021-09-22 22:01:03 +0800

    first commit

---
+++ text-code-c/homework.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main( void ){
+    printf("%s\n", "My name is nguyen thanh phuoc");
+    printf("%s\n", "My ID is D1072770");
+    printf("%s\n", "My major is information engineering and computer science");
+}
