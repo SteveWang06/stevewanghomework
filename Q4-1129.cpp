16cf0b0f37fd8284fe4bbe67b19d2fa654650e84
SteveWang06 <91189532+SteveWang06@users.noreply.github.com>
2021-11-29 18:27:09 +0800

    Add files via upload

---
+++ Q4-1129.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+void input(int *arr, int size = 0){
+	for(int i = 0; i <= size - 1; i++){
+		scanf("%d", &arr[i]);
+	}
+}
+
+int check(int *arr, int size, int x){
+	int count = 0;
+	for(int i = 0; i < size - 1; i++){
+		if(arr[i] == x){
+			count = i + 1;
+		}
+		
+	}
+	if(count > 0){
+		printf("%d position.", count);
+	}
+	else{
+		printf("%s", "none");
+	}
+}
+
+
+
+int main(){
+	int arr[100], size, x;
+	scanf("%d", &size);
+	input(arr, size);
+	scanf("%d", &x);
+	check(arr, size, x);
+	
+}
+
